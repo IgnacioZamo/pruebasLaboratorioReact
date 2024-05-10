4fbb9b88bdb2852cf3431f6f3ef3d98a4302f003
Ignacio Zamo <ignaciozamo1@gmail.com>
2024-05-10 19:41:29 -0300

    Se practica con los position absolute, relative

--- src/assets/components/EjerciciosTailwind/ContenedorTailwind.jsx
+++ src/assets/components/EjerciciosTailwind/ContenedorTailwind.jsx
@@ -1,8 +1,10 @@
+import Ej1 from "./ej1"
+
 
 const ContenedorTailwind = () => {
   return (
     <div className="w-4/5 mx-auto h-auto border border-blue-500 rounded-xl px-10 py-10">
-        <p>hola</p>
+        <Ej1/>
 
     </div>
   )
---
+++ src/assets/components/EjerciciosTailwind/ej1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ej1 = () => {
+  return (
+    <div className='w-4/5 h-72 mx-auto mt-3 border relative'>
+        <div className='w-48 h-48 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 border border-black rounded-md'>
+            <img className='absolute bottom-0' src="https://cdn.pixabay.com/photo/2020/05/07/19/46/hero-5142940_960_720.png" alt="" />
+
+        </div>
+
+    </div>
+  )
+}
+
+export default ej1

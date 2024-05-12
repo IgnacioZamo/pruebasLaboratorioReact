30f8c8ca16b0aacdb4f680b13c52d5ede5e8e2da
Ignacio Zamo <ignaciozamo1@gmail.com>
2024-05-11 21:49:09 -0300

    se practica layout utilizando positions absolute y relative

--- src/assets/components/EjerciciosTailwind/ej1.jsx
+++ src/assets/components/EjerciciosTailwind/ej1.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ej1 = () => {
   return (
     <div className='w-4/5 h-72 mx-auto mt-3 border relative'>
-        <div className='w-48 h-48 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 border border-black rounded-md'>
+        <div className='w-48 h-48 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition duration-300 ease-in-out hover:translate-x-1/4 border border-black rounded-md'>
             <img className='absolute bottom-0' src="https://cdn.pixabay.com/photo/2020/05/07/19/46/hero-5142940_960_720.png" alt="" />
 
         </div>

f9b2262a44b20b426cb731b1bc48e51f82a8aad2
Ignacio Zamo <ignaciozamo1@gmail.com>
2024-05-18 22:38:47 -0300

    Se comienza la seccion Hero de la tercera web

--- src/assets/components/EjerciciosTailwind/Ej4.jsx
+++ src/assets/components/EjerciciosTailwind/Ej4.jsx
@@ -17,6 +17,17 @@ const Ej4 = () => {
                 <button onClick={(e)=>e.preventDefault()} className='text-slate-600 text-sm font-bold px-3 py-2 border-2 border-lime-500 rounded-lg active:bg-lime-600 active:text-black '>Sign Up</button>
             </div>
         </navbar>
+        <div className='grid grid-cols-2 w-full mt-10 py-10'>
+            <div className='w-4/5 mx-auto'>
+                <h2 className='text-5xl text-transparent bg-clip-text bg-gradient-to-r from-slate-400 via-slate-200 to-slate-100 font-bold mb-10'>Let's explore your own <span className='text-lime-500'>VR</span> world</h2>
+                <p className='text-slate-100'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam praesentium consequuntur odio eum, impedit ex necessitatibus sed enim exercitationem nulla alias nihil aspernatur nesciunt? Similique!
+                </p>
+
+
+            </div>
+
+        </div>
 
     </div>
   )
